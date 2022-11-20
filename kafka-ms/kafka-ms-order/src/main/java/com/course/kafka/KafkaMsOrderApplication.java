ac22d389a258df26caf39f71074d64a01bf02f01
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2022-11-20 19:20:58 +0530

    Microservice Practiced

--- kafka-ms/kafka-ms-order/src/main/java/com/course/kafka/KafkaMsOrderApplication.java
+++ kafka-ms/kafka-ms-order/src/main/java/com/course/kafka/KafkaMsOrderApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KafkaMsOrderApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaMsOrderApplication.class, args);
 	}
-
 }
