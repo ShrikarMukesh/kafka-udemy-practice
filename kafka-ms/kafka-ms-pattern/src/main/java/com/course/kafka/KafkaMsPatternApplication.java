6f2a90d0c2a3a650f1113ada5f8d5a4d570b9490
Shrikar Mukesh <mukesh.shrikar7@gmail.com>
2023-01-01 18:54:41 +0530

    Kafka Pattern Practice

---
+++ kafka-ms/kafka-ms-pattern/src/main/java/com/course/kafka/KafkaMsPatternApplication.java
@@ -0,0 +1,11 @@
+package com.course.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaMsPatternApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMsPatternApplication.class, args);
+	}
+}
